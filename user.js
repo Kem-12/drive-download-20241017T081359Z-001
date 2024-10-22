20fd4379fd054a41e7725e56741482de046476c4
Kem-12 <d4v1dmw@outlook.com>
2024-10-22 07:12:31 -0400

    Initial commit

---
+++ user.js
@@ -0,0 +1,28 @@
+const postListEl = document.querySelector('.post-list');
+
+async function onSearchChange(event) {
+    const id = event.target.value;
+    renderPosts(id)
+}
+
+async function renderPosts(userId) {
+  const id = localStorage.getItem("id")
+  const posts = await fetch( `https://jsonplaceholder.typicode.com/posts?userId=${userId || id}`)
+  const postsData = await posts.json();
+
+  postListEl.innerHTML = postsData.map(post => postHTML(post)
+    ).join('');
+}
+
+function postHTML(post) {
+    return ` <div class="post">
+      <div class="post__title">
+        ${post.title}
+      </div>
+      <p class="post__body">
+        ${post.body}
+      </p>
+    </div>`
+}
+
+renderPosts(); 
